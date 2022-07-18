8ecbd996ddd3182872120b373cc4c371d5746ada
itzhak12380 <iascc12380@gmail.com>
2022-07-18 14:04:39 +0300

    cors policy check

--- server/server.js
+++ server/server.js
@@ -7,11 +7,25 @@ require('./DB/index')
 
 
 const app = express()
-app.use(cors())
+
 app.use(fileUpload({ createParentPath: true, useTempFiles: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+    }
+    next()
+})
 
 
 
