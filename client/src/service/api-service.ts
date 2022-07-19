213bb2aa027f593692be2ae7f88da46805886fde
itzhak12380 <iascc12380@gmail.com>
2022-07-19 06:56:11 +0300

    cors policy in check

--- client/src/service/api-service.ts
+++ client/src/service/api-service.ts
@@ -1,6 +1,6 @@
 export const API =
   process.env.NODE_ENV === "production"
-    ? `https://lifeproduct.herokuapp.com`
+    ? `https://ecommerce-kasie.herokuapp.com`
     : "http://localhost:8080";
 
 
--- client/src/service/productService.ts
+++ client/src/service/productService.ts
@@ -1,109 +1,66 @@
-import { API, GetErrorMessage } from "./api-service";
-import { IProduct, IProductImage } from "../Types/products";
+import { API, GetErrorMessage } from "./api-service"
+import {IProduct , IProductImage} from '../Types/products'
 
-export const getProduct = async (
-  page: number,
-  category: string,
-  sort: string,
-  search: string,
-  setproduct: Function,
-  setResult: Function
-) => {
-  try {
-    const res = await fetch(
-      `${API}/api/product?limit=${
-        page * 9
-      }&${category}&${sort}&title[regex]=${search}`,
-      {
-        method: "get",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.accessToken}`,
-          "Access-Control-Allow-Origin": "*",
+export const getProduct = async (page: number, category: string, sort: string, search: string, setproduct: Function, setResult: Function) => {
+    try {
+        const res = await fetch(`${API}/api/product?limit=${page * 9}&${category}&${sort}&title[regex]=${search}`, {
+            method: "get", headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.accessToken}`,
+            }
+        }).then(res => res.json()).then(responce => responce).catch(error => error)
+        setproduct(res.products);
+        setResult(res.result)
+        
+    } catch (error) {
+        GetErrorMessage(error)
+    }
+
+}
+
+
+export const deleteProduct = async (id: string | undefined, public_id: string) => {
+    try {
+        const destroyImage = fetch(`${API}/api/destroy`, {
+            method: "post", headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.accessToken}`
+            },
+            body: JSON.stringify({ public_id })
+        }).then(res => res.json()).then(responce => responce).catch(error => error)
+        const deleteProduct = fetch(`${API}/api/product/${id}`, {
+            method: "delete", headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.accessToken}`
+            }
+        }).then(res => res.json()).then(responce => responce).catch(error => error)
+        await destroyImage
+        await deleteProduct
+    } catch (error) {
+        GetErrorMessage(error)
+    }
+}
+
+
+
+export const updateProduct = async (product:IProduct,image:IProductImage,onEdit:boolean)=>{
+    if(onEdit){
+           await fetch(`${API}/api/product/${product._id}`, {
+        method: "put", headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.accessToken}`
         },
-      }
-    )
-      .then((res) => res.json())
-      .then((responce) => responce)
-      .catch((error) => error);
-    setproduct(res.products);
-    setResult(res.result);
-  } catch (error) {
-    GetErrorMessage(error);
-  }
-};
+        body: JSON.stringify({ ...product, image: {public_id:image.public_id,url:image.url} })
+    }).then(res => res.json()).then(responce => responce).catch(error => error)
+    }
+    else{
+        await fetch(`${API}/api/product`, {
+            method: "post", headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.accessToken}`
+            },
+            body: JSON.stringify({ ...product, image: {public_id:image.public_id,url:image.url} })
+        }).then(res => res.json()).then(responce => responce).catch(error => error)
 
-export const deleteProduct = async (
-  id: string | undefined,
-  public_id: string
-) => {
-  try {
-    const destroyImage = fetch(`${API}/api/destroy`, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.accessToken}`,
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({ public_id }),
-    })
-      .then((res) => res.json())
-      .then((responce) => responce)
-      .catch((error) => error);
-    const deleteProduct = fetch(`${API}/api/product/${id}`, {
-      method: "delete",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.accessToken}`,
-        "Access-Control-Allow-Origin": "*",
-      },
-    })
-      .then((res) => res.json())
-      .then((responce) => responce)
-      .catch((error) => error);
-    await destroyImage;
-    await deleteProduct;
-  } catch (error) {
-    GetErrorMessage(error);
-  }
-};
-
-export const updateProduct = async (
-  product: IProduct,
-  image: IProductImage,
-  onEdit: boolean
-) => {
-  if (onEdit) {
-    await fetch(`${API}/api/product/${product._id}`, {
-      method: "put",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.accessToken}`,
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({
-        ...product,
-        image: { public_id: image.public_id, url: image.url },
-      }),
-    })
-      .then((res) => res.json())
-      .then((responce) => responce)
-      .catch((error) => error);
-  } else {
-    await fetch(`${API}/api/product`, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.accessToken}`,
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({
-        ...product,
-        image: { public_id: image.public_id, url: image.url },
-      }),
-    })
-      .then((res) => res.json())
-      .then((responce) => responce)
-      .catch((error) => error);
-  }
-};
+    }
+}
